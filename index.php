4e62884d0c89dc1a6fc8a51af0ec8362467eb424
sarwar12 <sarwarkhan.nahin@gmail.com>
2024-10-28 14:40:13 +0600

    2nd commit

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ if(preg_match("~^/url_router/hello/(\w+)/?$~", $url, $matches)){
     echo $matches[1];
 }
 //Homepage match with RegExp
-if(preg_match("~^/url_router/$~", $url)){
+if(preg_match("~^/$~", $url)){
     echo "Welcome Home";
 }
+//Match with number
+if(preg_match("~^/url_router/number/(\d+)/do/(\w+)/?$~", $url, $matches)){
+    echo $matches[1];
+    echo "<br/>";
+    echo $matches[2];
+}
+
